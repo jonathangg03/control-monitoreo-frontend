50ecd4f712f6e7178ad98e72292bf9bff5949b1a
Jonathan García <jona03g97@gmail.com>
2022-03-09 20:33:00 -0600

    Delete: Styles files in pages

--- pages/equipo/index.js
+++ pages/equipo/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import ReplacementList from '../../components/ReplacementList'
 import Title from '../../components/Title'
-import styles from './styles'
+import { colors, fontSizes } from '../../styles/themes'
 
 const Keys = () => {
   return (
@@ -21,7 +21,42 @@ const Keys = () => {
       <Link href='/equipo/nuevo'>
         <a>Agregar registro</a>
       </Link>
-      <style jsx>{styles}</style>
+      <style jsx>{`
+        a {
+          font-size: ${fontSizes.main};
+          color: ${colors.selector};
+          text-decoration: none;
+        }
+
+        form {
+          margin-bottom: 20px;
+        }
+
+        input {
+          width: 50%;
+          height: 35px;
+          margin-bottom: 10px;
+          outline: none;
+          padding: 0px 5px;
+          border: none;
+          border-bottom: 1px solid #f1f1f1;
+          color: ${colors.main};
+        }
+
+        input::placeholder {
+          color: #c1c1c1;
+        }
+
+        button {
+          display: block;
+          background-color: ${colors.selector};
+          border: none;
+          color: white;
+          padding: 10px 40px;
+          border-radius: 5px;
+          font-size: ${fontSizes.main};
+        }
+      `}</style>
     </>
   )
 }
